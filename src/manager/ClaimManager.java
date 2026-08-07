3585c5afbccd7c1243a4c2feed6d1116e2f6c5fe
Nguyen Khanh Nguyen <s4197203@rmit.edu.vn>
2026-08-07 12:00:58 +0700

    clean up unused imports in ClaimManager

--- src/manager/ClaimManager.java
+++ src/manager/ClaimManager.java
@@ -8,10 +8,6 @@ import model.InsuranceCard;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Centralized data manager for ClaimShield system.
